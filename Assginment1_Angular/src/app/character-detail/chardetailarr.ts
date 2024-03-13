e1c79ec5ec68f8ee1b09ca0b43c5b0b0df82a942
vansh8299 <76584561+vansh8299@users.noreply.github.com>
2024-03-13 12:51:02 +0530

    CSS Updated

--- Assginment1_Angular/src/app/character-detail/chardetailarr.ts
+++ Assginment1_Angular/src/app/character-detail/chardetailarr.ts
@@ -1,8 +1,8 @@
 import { chardetailInter } from './chardetailInterface';
 
 export const chardetailarr: chardetailInter[] = [
-  { name: 'Nobita', charid: 1, height: 1, weight: 25 },
-  { name: 'Sunio', charid: 2, height: 1, weight: 25 },
-  { name: 'Shizhuka', charid: 3, height: 1, weight: 25 },
-  { name: 'Jeean', charid: 4, height: 1, weight: 25 },
+  { name: 'Nobita', charid: 1, height: 4.3, weight: 40 },
+  { name: 'Sunio', charid: 2, height: 3.4, weight: 30 },
+  { name: 'Shizhuka', charid: 3, height: 4.2, weight: 35 },
+  { name: 'Jeean', charid: 4, height: 5, weight: 500 },
 ];
